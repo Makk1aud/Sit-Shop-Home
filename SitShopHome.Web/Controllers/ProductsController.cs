067b92adde0407fe3f7052eada35a58f93fb0742
Baturhan <baturxan-06.72@mail.ru>
2023-12-07 17:49:27 +0600

    Adding products controller

---
+++ SitShopHome.Web/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SitShopHome.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
